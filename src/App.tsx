230ab6317a4647afcdfcdecb01b2068e17dd5d1d
Oleksandr-67743 <oandrii3@stu.vistula.edu.pl>
2026-02-16 18:49:35 +0100

    feat: add mock ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import Orb from '@/components/orb';
+import BlurText from '@/components/blur-text';
 
 function App() {
-  return <main></main>;
+  return (
+    <main className="min-h-screen w-full flex flex-col justify-center items-center bg-black">
+      <Orb hoverIntensity={2} rotateOnHover hue={0} forceHoverState={false} backgroundColor="#000000" />
+      <BlurText text="Hello World" animateBy="letters" className="text-white" />
+    </main>
+  );
 }
 
 export default App;
